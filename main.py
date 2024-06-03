6e6002e34d35da3f0663ee207f1a227d7bb8755d
Dev. Sanches <dev.anaclarasanches@gmail.com>
2024-06-03 07:38:07 -0300

    back end to convert

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import streamlit as st
+from PIL import Image
+# biblioteca incluida em streamlit
 
-st.camera_input("Camera")
+image = st.camera_input("Camera")
+print(image)
+
+image_obj = Image.open(image)
+gray_img = image_obj.convert("L")
