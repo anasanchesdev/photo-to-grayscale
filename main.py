af6a59c8f84e2e40d35eaca927cde20f6979a456
Dev. Sanches <dev.anaclarasanches@gmail.com>
2024-06-03 08:36:03 -0300

    upload image option

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import streamlit as st
 from PIL import Image  # biblioteca incluida em streamlit
 
+
+def process_image(image):
+    image_obj = Image.open(image)  # cria objeto de imagem pillow
+    gray_img = image_obj.convert('L')  # converte para cinza
+    st.image(gray_img)  # mostra a imagem em cinza
+
+
 st.title("Selfie to Grayscale")
 st.subheader("A simple program that converts a selfie or photo to grayscale")
+uploaded_image = st.file_uploader("Upload Image")
 
 with st.expander("Run Camera"):  # botão/ação para iniciar a camera
     # inicia prompt da camera
-    image = st.camera_input("Camera")
+    camera_image = st.camera_input("Camera")
 
-if image:  # após o usuário permitir o acesso à camera
-    # cria objeto de imagem pillow
-    image_obj = Image.open(image)
+if camera_image:  # após o usuário permitir o acesso à camera
+    process_image(camera_image)
 
-    # converte para cinza
-    gray_img = image_obj.convert('L')
-
-    # mostra a imagem em cinza
-    st.image(gray_img)
+elif uploaded_image:  # após o usuário fazer upload de alguma foto
+    process_image(uploaded_image)
